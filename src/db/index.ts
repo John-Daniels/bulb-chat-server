9d1a54127386ea430b424e44ff3fc047a3bb39c1
John Daniels Adeyemi <adeyemijohndaniels@gmail.com>
2022-11-10 17:23:06 +0100

    cleanup project

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,12 @@
 import mongoose from "mongoose";
 
-const main = async (): Promise<void> => {
-    //this is used in our local environment
-    try {
-        //                         main_atlas_url            || fallback to test_uri - when testing
-        const _uri: string = (process.env.MONGODB_URI || process.env.TEST_URI) as string;
-
-        await mongoose.connect(_uri);
-
+const main = () => {
+    const _uri: string = (process.env.MONGODB_URI) as string;
+    mongoose.connect(_uri, (err) => {
+        if (err) return console.log("Error connecting to the database!!!");
         console.log("Successfully connected to the database!");
-    } catch (e) {
-        console.log("Error connecting to the database!!!");
-    }
+    });
+
 };
 
 main();
